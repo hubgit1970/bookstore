ac7ec2621063aa654e9f1e18f0c45a769eaf6513
chrismulraney <searchlumin70@gmail.com>
2019-08-21 10:44:40 -0700

    Added Book model, Book admin class, change store template

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path
+from django.contrib import admin
+from . import urls
+
 
 urlpatterns = [
+    url(r'^store/', include('store.urls'), name='store'),
+    # url(r'^admin/', include(admin.site.urls), name='admin'),
     path('admin/', admin.site.urls),
 ]
